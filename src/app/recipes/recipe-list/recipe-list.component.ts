7b1950e28808fc1891e05e710f2108c3ee443285
Fazhrul Islam Sadip <86272587+sadipnsu@users.noreply.github.com>
2022-11-02 12:31:20 +0600

    Complete

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,11 +9,15 @@ import { Recipe } from '../recipe.modtel';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('A test recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A test recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A test recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+
   ];
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients:Ingredient[] = [ 
+    new Ingredient('Apples',5),
+    new Ingredient('Tomatoes',10),
+    new Ingredient('Banana',15),
+    new Ingredient('Mango',20),
+    new Ingredient('Guava',30),
+    new Ingredient('Pineapple',50),
+    new Ingredient('Grapes',100),
+    new Ingredient('Watermelon',200),
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
